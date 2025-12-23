3a60b1a082e23a8ea18d0709fe9ea23bd0ae82b5
Higor Milani <milanihigor016@gmail.com>
2025-12-23 18:39:38 -0300

    refactor: simplify imports and improve component naming in Contato.jsx; optimize CSS padding values

--- src/sections/Contato.jsx
+++ src/sections/Contato.jsx
@@ -1,30 +1,17 @@
-import React, { useEffect } from 'react';
+import 'react';
 
-import ScrollReveal from "scrollreveal";
 
-import Section_Header from '../components/Sections_Header';
+import SectionHeader from '../components/Sections_Header';
 
 import styles from "./Contato.module.css";
 
 
 const Contato = () =>{
-
-    /*
-    useEffect(() => {
-        ScrollReveal().reveal(".scroll_conteiner", {
-            distance: '100px',
-            duration: 2000,
-            delay: 10,
-            origin: 'top'
-        });
-    }, []);
-    */
-
-
+    
     return(
         <section id="contato" className={styles.contato} >
             <div className='scroll_conteiner'>
-                <Section_Header title={"Contate "} span={"Me!"}/>
+                <SectionHeader title={"Contate "} span={"Me!"}/>
                 <p>Ficarei feliz em ouvir de você e discutir sobre oportunidades de colaboração. Por favor, utilize os botões abaixo para me contatar por e-mail ou acessar meu perfil no LinkedIn.  </p>
                 <div className={styles.contato_conteiner}>   
                         <a href="https://www.linkedin.com/in/higor-milani/overlay/contact-info/" target="_blank" rel="noopener noreferrer">Linkedin</a>
