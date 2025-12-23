dc0b84c0ed2e3a6da9fd9511660594ff53f176bf
Higor Milani <milanihigor016@gmail.com>
2025-12-23 18:40:15 -0300

    refactor: enhance Btn component with prop validation and improve link/button rendering; update Card components to include rel="noreferrer"

--- src/components/Card_Portfolio/index.jsx
+++ src/components/Card_Portfolio/index.jsx
@@ -1,5 +1,6 @@
 import styles from "./style.module.css";
 
+// eslint-disable-next-line react/prop-types
 const Card_Portfolio = ({img, title, description, url, download=null }) => {
     return(
         <div className={styles.Card_Portfolio}>
@@ -7,7 +8,7 @@ const Card_Portfolio = ({img, title, description, url, download=null }) => {
             <div className={styles.Card_Portfolio_conteiner}>
                 <h4>{title}</h4>
                 <p>{description}</p>
-                <a href={url} target="_blank" download={download}><i className='bx bx-link-external'></i></a>
+                <a href={url} target="_blank" download={download} rel="noreferrer"><i className='bx bx-link-external'></i></a>
             </div>
         </div>
     )
