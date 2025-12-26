0461c208908942f9dc06926b05d8865376f8a618
Higor Milani <milanihigor016@gmail.com>
2025-12-26 12:02:21 -0300

    fix: update copyright year in footer to 2026

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ const Footer = () =>{
     return(
         <footer className={styles.footer}>
             <div className={styles.footer_text}>
-                <p>Copyright &copy; 2024. Todos direitos reservados!</p>
+                <p>Copyright &copy; 2026. Todos direitos reservados!</p>
             </div>
             <div className={styles.footer_icon}>
                 <a href="#home"><i className='bx bxs-caret-up-circle'></i></a>
