0374faf99d84fa769e295734f4e559ef9cff92b2
Higor Milani <milanihigor016@gmail.com>
2025-12-29 18:48:22 -0300

    feat: add multilingual support to Contact section with dynamic content rendering

--- src/sections/Contato.jsx
+++ src/sections/Contato.jsx
@@ -1,4 +1,5 @@
 import 'react';
+import {useTranslation} from "react-i18next";
 
 
 import SectionHeader from '../components/Sections_Header';
@@ -7,15 +8,16 @@ import styles from "./Contato.module.css";
 
 
 const Contato = () =>{
-    
+    const { t } = useTranslation();
+
     return(
         <section id="contato" className={styles.contato} >
             <div className='scroll_conteiner'>
-                <SectionHeader title={"Contate "} span={"Me!"}/>
-                <p>Ficarei feliz em ouvir de você e discutir sobre oportunidades de colaboração. Por favor, utilize os botões abaixo para me contatar por e-mail ou acessar meu perfil no LinkedIn.  </p>
-                <div className={styles.contato_conteiner}>   
-                        <a href="https://www.linkedin.com/in/higor-milani/overlay/contact-info/" target="_blank" rel="noopener noreferrer">Linkedin</a>
-                        <a href="mailto:milanihigor016@gmail.com">Email</a>
+                <SectionHeader title={t('contact.title') + " "} span={t('contact.span')}/>
+                <p>{t('contact.description')}</p>
+                <div className={styles.contato_conteiner}>
+                        <a href="https://www.linkedin.com/in/higor-milani/overlay/contact-info/" target="_blank" rel="noopener noreferrer">{t('contact.linkedin_btn')}</a>
+                        <a href="mailto:milanihigor016@gmail.com">{t('contact.email_btn')}</a>
                 </div>
             </div>
         </section>
